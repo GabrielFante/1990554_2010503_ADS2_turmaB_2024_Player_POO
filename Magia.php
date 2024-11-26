8cfcffea4c806dbe3ef3f93e3a177630e712f247
GabrielFante <164691063+GabrielFante@users.noreply.github.com>
2024-11-25 22:01:52 -0300

    Update Magia.php

---
+++ Magia.php
@@ -0,0 +1,12 @@
+<?php
+
+require_once('Item.php');
+
+class Magia extends Item
+{
+    public function __construct(string $name, int $tamanho, string $classe){
+        parent:: __construct($name, $tamanho, $classe);
+        $this->setTamanho(2);
+        $this->setClasse('Magia');
+    }
+}
